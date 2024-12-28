e7f542805e30e9dd83bf63a73ddcf4f713963609
thefeqy <thefeqy@gmail.com>
2024-12-28 17:28:46 +0100

    useMemo Hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
+import { initialItems } from "./_utils";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [items] = useState(initialItems);
 
-  useEffect(() => {
-    // Some Sort Of Action
-
-    console.log("The count is: ", count);
-
-    // (Optional) Cleanup
-
-    return () => {
-      console.log("Clean up");
-    };
-  }, [count]); // The Dependency Array
+  const selectedItem = useMemo(
+    () => items.find((item) => item.isSelected),
+    [items]
+  );
 
   return (
     <div className="App">
       <h2>Count: {count}</h2>
+      <h2>selected Item: {selectedItem?.id}</h2>
 
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
