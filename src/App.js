0d8f00b8d157482a6f00ba025d467cfae60d5eb3
thefeqy <thefeqy@gmail.com>
2024-12-28 17:00:41 +0100

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    // Some Sort Of Action
+
+    console.log("The count is: ", count);
+
+    // (Optional) Cleanup
+
+    return () => {
+      console.log("Clean up");
+    };
+  }, [count]); // The Dependency Array
+
   return (
     <div className="App">
       <h2>Count: {count}</h2>
-      
+
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
