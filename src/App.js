b599d14446f3b10741ae49d746b95338a453ac60
thefeqy <thefeqy@gmail.com>
2024-12-28 16:35:35 +0100

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h2>Welcome</h2>
+      <h2>Count: {count}</h2>
+      
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
 }
